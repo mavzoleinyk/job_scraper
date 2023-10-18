4c80c36bc3553cef860f10dccd964f7fc18a6d21
mavzoleinyk <itstart.com.ua@gmail.com>
2023-10-19 02:15:57 +0300

    Пaрсер сайта rabota.ru

--- src/scraping/parsers.py
+++ src/scraping/parsers.py
@@ -108,10 +108,36 @@ def djinni(url):
         errors.append({'url': url, 'title': "Page do not response"})
     return jobs, errors
 
+def rabotaru(url):
+    jobs = []
+    errors = []
+    domain = 'https://rostov.rabota.ru'
+    resp = requests.get(url, headers=headers)
+    if resp.status_code == 200:
+        soup = BS(resp.content, 'html.parser')
+        main_list = soup.find('div', attrs={'class': 'r-serp__infinity-list'})
+        if main_list:
+            art_lst = main_list.find_all('article', attrs={'class': 'vacancy-preview-card'})
+            for art in art_lst:
+                title = art.find('h3', attrs={'class': 'vacancy-preview-card__title'})
+                href = title.a['href']
+                cont = art.find('div', attrs={'class': 'vacancy-preview-card__short-description'})
+                content = cont.text
+                company = 'No name'
+                comp = art.find('span', attrs={'class': 'vacancy-preview-card__company-name'})
+                if comp:
+                    company = comp.a.text
+                jobs.append({'title': title.text, 'url': domain + href, 'description': content, 'company': company})
+        else:
+            errors.append({'url': url, 'title': "Div does not exists"})
+    else:
+        errors.append({'url': url, 'title': "Page do not response"})
+    return jobs, errors
+
 
 if __name__ == '__main__':
-    url = 'https://djinni.co/jobs/?primary_keyword=Python&region=UKR&location=kyiv'
-    jobs, errors = djinni(url)
+    url = 'https://rostov.rabota.ru/vacancy/?query=python&location.ll=47.222078,39.720358&location.kind=region&location.radius=any&location.regionId=284&location.name=%D0%A0%D0%BE%D1%81%D1%82%D0%BE%D0%B2-%D0%BD%D0%B0-%D0%94%D0%BE%D0%BD%D1%83&sort=distance'
+    jobs, errors = rabotaru(url)
     h = codecs.open('work.txt', 'w', 'utf-8')
     h.write(str(jobs))
     h.close()
