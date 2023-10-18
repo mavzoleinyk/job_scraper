66a59e2ef1710cb2c34dfb3d38e067db67139912
mavzoleinyk <itstart.com.ua@gmail.com>
2023-10-19 02:35:28 +0300

    Отдельный файл для запуска всех функций парсера

--- src/scraping/parsers.py
+++ src/scraping/parsers.py
@@ -1,8 +1,18 @@
 import requests
 import codecs
 from bs4 import BeautifulSoup as BS
+from random import randint
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv: 47.0) Gecko/20100101 Firefox/47.0', 'Accept': 'text/html, application/xhtml+xml,     application/xml; q=0.9,*/*;q=0.8'}
+__all__ = ('work', 'rabota', 'dou', 'djinni', 'rabotaru')
+
+headers = [
+    {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:47.0) Gecko/20100101 Firefox/47.0',
+        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'},
+    {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36',
+        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'},
+    {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; rv:53.0) Gecko/20100101 Firefox/53.0',
+        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
+    ]
 
 
 def work(url):
@@ -10,7 +20,7 @@ def work(url):
     errors = []
     domain = 'https://www.work.ua'
     url = 'https://www.work.ua/ru/jobs-kyiv-python/'
-    resp = requests.get(url, headers=headers)
+    resp = requests.get(url, headers=headers[randint(0,2)])
     if resp.status_code == 200:
         soup = BS(resp.content, 'html.parser')
         main_div = soup.find('div', id='pjax-job-list')
@@ -35,7 +45,7 @@ def rabota(url):
     jobs = []
     errors = []
     domain = 'https://robota.ua'
-    resp = requests.get(url, headers=headers)
+    resp = requests.get(url, headers=headers[randint(0,2)])
     if resp.status_code == 200:
         soup = BS(resp.content, 'html.parser')
         section_div = soup.find('section', attrs={'class': 'ng-star-inserted'})
@@ -60,7 +70,7 @@ def dou(url):
     jobs = []
     errors = []
     #domain = 'https://www.work.ua'
-    resp = requests.get(url, headers=headers)
+    resp = requests.get(url, headers=headers[randint(0,2)])
     if resp.status_code == 200:
         soup = BS(resp.content, 'html.parser')
         main_div = soup.find('div', id='vacancyListId')
@@ -86,7 +96,7 @@ def djinni(url):
     jobs = []
     errors = []
     domain = 'https://djinni.co'
-    resp = requests.get(url, headers=headers)
+    resp = requests.get(url, headers=headers[randint(0,2)])
     if resp.status_code == 200:
         soup = BS(resp.content, 'html.parser')
         main_ul = soup.find('ul', attrs={'class': 'list-jobs'})
@@ -112,7 +122,7 @@ def rabotaru(url):
     jobs = []
     errors = []
     domain = 'https://rostov.rabota.ru'
-    resp = requests.get(url, headers=headers)
+    resp = requests.get(url, headers=headers[randint(0,2)])
     if resp.status_code == 200:
         soup = BS(resp.content, 'html.parser')
         main_list = soup.find('div', attrs={'class': 'r-serp__infinity-list'})
