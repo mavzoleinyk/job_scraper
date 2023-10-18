399398fa6b1972efa94b896742313c8619c1cfc8
mavzoleinyk <itstart.com.ua@gmail.com>
2023-10-19 00:47:04 +0300

    Функция парсинга rabota.ua работает через ВПН и фронт через js шляпу

--- src/work.py
+++ src/work.py
@@ -4,31 +4,61 @@ from bs4 import BeautifulSoup as BS
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv: 47.0) Gecko/20100101 Firefox/47.0', 'Accept': 'text/html, application/xhtml+xml,     application/xml; q=0.9,*/*;q=0.8'}
 
-domain = 'https://www.work.ua'
-url = 'https://www.work.ua/ru/jobs-kyiv-python/'
-resp = requests.get(url, headers=headers)
-jobs = []
-errors = []
-if resp.status_code == 200:
-    soup = BS(resp.content, 'html.parser')
-    main_div = soup.find('div', id='pjax-job-list')
-    if main_div:
-        div_lst = main_div.find_all('div', attrs={'class': 'job-link'})
-        for div in div_lst:
-            title = div.find('h2')
-            href = title.a['href']
-            content = div.p.text
-            company = 'No name'
-            logo = div.find('img')
-            if logo:
-                company = logo['alt']
-            jobs.append({'title': title.text, 'url': domain + href, 'description': content, 'company': company})
+
+def work(url):
+    jobs = []
+    errors = []
+    domain = 'https://www.work.ua'
+    url = 'https://www.work.ua/ru/jobs-kyiv-python/'
+    resp = requests.get(url, headers=headers)
+    if resp.status_code == 200:
+        soup = BS(resp.content, 'html.parser')
+        main_div = soup.find('div', id='pjax-job-list')
+        if main_div:
+            div_lst = main_div.find_all('div', attrs={'class': 'job-link'})
+            for div in div_lst:
+                title = div.find('h2')
+                href = title.a['href']
+                content = div.p.text
+                company = 'No name'
+                logo = div.find('img')
+                if logo:
+                    company = logo['alt']
+                jobs.append({'title': title.text, 'url': domain + href, 'description': content, 'company': company})
+        else:
+            errors.append({'url': url, 'title': "Div does not exists"})
     else:
-        errors.append({'url': url, 'title': "Div does not exists"})
-else:
-    errors.append({'url': url, 'title': "Page do not response"})
+        errors.append({'url': url, 'title': "Page do not response"})
+    return jobs, errors
 
+def rabota(url):
+    jobs = []
+    errors = []
+    domain = 'https://robota.ua'
+    resp = requests.get(url, headers=headers)
+    if resp.status_code == 200:
+        soup = BS(resp.content, 'html.parser')
+        section_div = soup.find('section', attrs={'class': 'ng-star-inserted'})
+        if section_div:
+            vacancies_list = section_div.find_all('div', attrs={'class': 'ng-star-inserted'})
+            for div in vacancies_list:
+                title = div.find('h2')
+                href = vacancies_list.a['href']
+                content = div.p.text
+                company = 'No name'
+                logo = div.find('img')
+                if logo:
+                    company = logo['alt']
+                jobs.append({'title': title.text, 'url': domain + href, 'description': content, 'company': company})
+        else:
+            errors.append({'url': url, 'title': "Section does not exists"})
+    else:
+        errors.append({'url': url, 'title': "Page do not response"})
+    return jobs, errors
 
-h = codecs.open('work.txt', 'w', 'utf-8')
-h.write(str(jobs))
-h.close()
+if __name__ == '__main__':
+    url = 'https://robota.ua/zapros/python/kyiv'
+    jobs, errors = rabota(url)
+    h = codecs.open('work.txt', 'w', 'utf-8')
+    h.write(str(jobs))
+    h.close()
