a7bc6e7b756ef9367190f91ba9acdc760e9070f7
mavzoleinyk <itstart.com.ua@gmail.com>
2023-10-21 11:06:05 +0300

    Настройка запуска django вне django

--- src/run_scraping.py
+++ src/run_scraping.py
@@ -1,5 +1,16 @@
 import codecs
+import os, sys
+
+proj = os.path.dirname(os.path.abspath('manage.py'))
+sys.path.append(proj)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'scrap_service.settings'
+
+import django
+django.setup()
+
 from scraping.parsers import*
+
+from scraping.models import Vacancy, City, Language
 
 
 parsers = (
@@ -9,7 +20,7 @@ parsers = (
     (djinni, 'https://djinni.co/jobs/?primary_keyword=Python&region=UKR&location=kyiv'),
     (rabotaru, 'https://rostov.rabota.ru/vacancy/?query=python&location.ll=47.222078,39.720358&location.kind=region&location.radius=any&location.regionId=284&location.name=%D0%A0%D0%BE%D1%81%D1%82%D0%BE%D0%B2-%D0%BD%D0%B0-%D0%94%D0%BE%D0%BD%D1%83&sort=distance')
     )
-
+city = City.objects.filter(slug='kiev') 
 jobs, errors = [], []
 for func, url in parsers:
     j, e = func(url)
