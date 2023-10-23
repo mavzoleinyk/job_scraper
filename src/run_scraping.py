57d4c6cdbe53d142470f0d6b6005228bbd9a2a7d
mavzoleinyk <itstart.com.ua@gmail.com>
2023-10-23 23:09:30 +0300

    Мелкие фиксы #3

--- src/run_scraping.py
+++ src/run_scraping.py
@@ -18,11 +18,11 @@ from scraping.models import Vacancy, City, Language, Error, Url
 User = get_user_model()
 
 parsers = (
-    (work, 'https://www.work.ua/ru/jobs-kyiv-python/'),
-    (rabota, 'https://robota.ua/zapros/python/kyiv'),
-    (dou, 'https://jobs.dou.ua/vacancies/?city=%D0%9A%D0%B8%D1%97%D0%B2&category=Python'),
-    (djinni, 'https://djinni.co/jobs/?primary_keyword=Python&region=UKR&location=kyiv'),
-    (rabotaru, 'https://rostov.rabota.ru/vacancy/?query=python&location.ll=47.222078,39.720358&location.kind=region&location.radius=any&location.regionId=284&location.name=%D0%A0%D0%BE%D1%81%D1%82%D0%BE%D0%B2-%D0%BD%D0%B0-%D0%94%D0%BE%D0%BD%D1%83&sort=distance')
+    (work, 'work'),
+    (rabota, 'rabota'),
+    (dou, 'dou'),
+    (djinni, 'djinni'),
+    (rabotaru, 'rabotaru')
     )
 
 def get_settings():
